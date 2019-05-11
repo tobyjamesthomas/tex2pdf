d5ca0774d26b88bf51a09203e2a86f6e34b6c33b
Toby <toby.james.thomas@gmail.com>
2019-05-11 16:23:24 -0400

    Run pdflatex then move log files to extra folder, since .tex file may reference other files and images in parent folder

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -8,19 +8,21 @@
 # Exit on error
 set -e
 
-# First cd to extra folder
+# First create extra folder for logs
 mkdir -p extra
-cd extra
 
 # Run pdflatex, convert LaTeX to PDF
-pdflatex ./../$1.tex
+pdflatex $1.tex
 
-# Move PDF to back to parent folder
-mv $1.pdf ./..
-cd ..
+# Move logs to extra folder
+# *.log targets $1.log and texput.log (if error)
+mv $1.aux $1.out *.log extra/
 
-# Copy PDF to ~/public_html/cs240
+# Copy PDF to ~/public_html/cs240/
 cp $1.pdf $HOME/public_html/cs240/$1.pdf
+
+# Allow read permissions for all groups
+chmod a+r $HOME/public_html/cs240/$1.pdf
 
 echo -e "____________________________________________________________________\n"
 echo "PDF is live at: https://www.student.cs.uwaterloo.ca/~${HOME##*/}/cs240/$1.pdf"
